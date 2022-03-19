bdb04fef84f04884779a0e47ed05c78514e5a509
Erik Aker <eraker@gmail.com>
2022-03-19 12:02:50 -0700

    Fix logging

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use anyhow::Context;
 use clap::Parser;
 use sqlx::postgres::PgPoolOptions;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::{EnvFilter, Registry};
 
 use hooksaurus_auctions::config::Config;
 use hooksaurus_auctions::endpoints;
@@ -10,7 +14,16 @@ async fn main() -> anyhow::Result<()> {
     if std::env::var_os("RUST_LOG").is_none() {
         std::env::set_var("RUST_LOG", "hooksaurus_auctions=debug,tower_http=debug")
     }
-    tracing_subscriber::fmt::init();
+    LogTracer::init().expect("Unable to setup log tracer!");
+
+    let app_name = concat!(env!("CARGO_PKG_NAME"), "-", env!("CARGO_PKG_VERSION")).to_string();
+    let (non_blocking_writer, _guard) = tracing_appender::non_blocking(std::io::stdout());
+    let bunyan_formatting_layer = BunyanFormattingLayer::new(app_name, non_blocking_writer);
+    let subscriber = Registry::default()
+        .with(EnvFilter::new("INFO"))
+        .with(JsonStorageLayer)
+        .with(bunyan_formatting_layer);
+    tracing::subscriber::set_global_default(subscriber).unwrap();
 
     let config = Config::parse();
     let db = PgPoolOptions::new()
